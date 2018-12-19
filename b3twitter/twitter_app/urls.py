212508c5765c49a815248211c9657a6b2d17b75e
nocox <tutinoko79@gmail.com>
2018-12-20 02:22:03 +0900

    fix tutorial_2

--- b3twitter/twitter_app/urls.py
+++ b3twitter/twitter_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.top, name='top'),
     path('home', views.home, name='home'),
     path('create_tweet', views.create_tweet, name='create_tweet'),
+    path('profile/<str:user_name>', views.profile, name='profile') # 追加
 ]
--- b3twitter/twitter_app/views.py
+++ b3twitter/twitter_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Tweet
+from .models import User
 from .forms import TweetForm
 
 
@@ -25,4 +26,13 @@ def create_tweet(request):
     if new_tweet.is_valid():
         tweet = Tweet(user=request.user, content=new_tweet.instance.content)
         tweet.save()
+    return redirect(home)
+
+def profile(request, user_name):
+    target_user = User.objects.filter(username=user_name).first()
+    if target_user:
+        tweets = Tweet.objects.filter(user__username=target_user.username).order_by("created_at").reverse()
+        queries = {'target_user': target_user, 'tweets': tweets}
+        return render(request, 'twitter_app/profile.html', queries)
+
     return redirect(home)
